319c2bee40ff0b7640193750a423d5ebb5ca4d55
aditya0kmr <69079096+aditya0kmr@users.noreply.github.com>
2025-11-27 09:06:32 +0530

    Create FavoritesPage.jsx

---
+++ src/pages/FavoritesPage.jsx
@@ -0,0 +1,10 @@
+function FavoritesPage() {
+  return (
+    <div className="page favorites-page">
+      <h1>Favorites</h1>
+      <p>Saved memories and moments - Coming soon...</p>
+    </div>
+  )
+}
+
+export default FavoritesPage
