393764fc8945305c47736ff669f2ba299a1bf7f9
aditya0kmr <69079096+aditya0kmr@users.noreply.github.com>
2025-11-27 09:00:24 +0530

    Create TimelinePage.jsx

---
+++ src/pages/TimelinePage.jsx
@@ -0,0 +1,10 @@
+function TimelinePage() {
+  return (
+    <div className="page timeline-page">
+      <h1>Our Timeline</h1>
+      <p>Our journey milestones - Coming soon...</p>
+    </div>
+  )
+}
+
+export default TimelinePage
