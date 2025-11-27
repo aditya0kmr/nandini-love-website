1ec27f23c64ff5b13b246fd75817fdbeb4693e9d
aditya0kmr <69079096+aditya0kmr@users.noreply.github.com>
2025-11-27 08:59:24 +0530

    Create GalleryPage.jsx

---
+++ src/pages/GalleryPage.jsx
@@ -0,0 +1,10 @@
+function GalleryPage() {
+  return (
+    <div className="page gallery-page">
+      <h1>Our Gallery</h1>
+      <p>3D carousel memories - Coming soon...</p>
+    </div>
+  )
+}
+
+export default GalleryPage
