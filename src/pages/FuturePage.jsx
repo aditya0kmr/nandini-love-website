6a7c32dc498005d0d72215ef212856b483034c6a
aditya0kmr <69079096+aditya0kmr@users.noreply.github.com>
2025-11-27 09:05:02 +0530

    Create FuturePage.jsx

---
+++ src/pages/FuturePage.jsx
@@ -0,0 +1,10 @@
+function FuturePage() {
+  return (
+    <div className="page future-page">
+      <h1>Our Future</h1>
+      <p>Dreams and plans ahead - Coming soon...</p>
+    </div>
+  )
+}
+
+export default FuturePage
