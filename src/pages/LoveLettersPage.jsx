95cce94dbcbdb01655083400d3ff4b3e55dc8d68
aditya0kmr <69079096+aditya0kmr@users.noreply.github.com>
2025-11-27 08:58:26 +0530

    Create LoveLettersPage.jsx

---
+++ src/pages/LoveLettersPage.jsx
@@ -0,0 +1,10 @@
+function LoveLettersPage() {
+  return (
+    <div className="page letters-page">
+      <h1>Love Letters</h1>
+      <p>Sweet and flirty letters - Coming soon...</p>
+    </div>
+  )
+}
+
+export default LoveLettersPage
