6cd112278d9848c2dee7227867a3fcd78c3a2df0
aditya0kmr <69079096+aditya0kmr@users.noreply.github.com>
2025-11-27 09:03:36 +0530

    Create GamesPage.jsx

---
+++ src/pages/GamesPage.jsx
@@ -0,0 +1,10 @@
+function GamesPage() {
+  return (
+    <div className="page games-page">
+      <h1>Play Games</h1>
+      <p>Fun couple games - Coming soon...</p>
+    </div>
+  )
+}
+
+export default GamesPage
