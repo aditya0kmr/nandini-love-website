dfda5703ab69e1dd35649e39325afc70675b4918
aditya0kmr <69079096+aditya0kmr@users.noreply.github.com>
2025-11-27 09:01:38 +0530

    Create PoemsQuotesPage.jsx

---
+++ src/pages/PoemsQuotesPage.jsx
@@ -0,0 +1,10 @@
+function PoemsQuotesPage() {
+  return (
+    <div className="page poems-page">
+      <h1>Poems & Quotes</h1>
+      <p>Love poems and quotes - Coming soon...</p>
+    </div>
+  )
+}
+
+export default PoemsQuotesPage
