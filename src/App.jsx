a366be91272a17ddddb511ca7349980dfe2d6747
aditya0kmr <69079096+aditya0kmr@users.noreply.github.com>
2025-12-14 03:56:27 +0530

    feat: integrate ThemeProvider and responsive styles into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
+import { ThemeProvider } from './context/ThemeContext'
 
 // Page imports
 import LoginPage from './pages/LoginPage'
@@ -15,27 +16,33 @@ import HerCornerPage from './pages/HerCornerPage'
 // Component imports
 import FloatingNav from './components/FloatingNav'
 import ComplimentSystem from './components/ComplimentSystem'
+
+// Style imports
+import './styles/theme.css'
+import './styles/responsive.css'
 import './styles/ComplimentSystem.css'
 
 function App() {
   return (
-    <div className="app">
-      <FloatingNav />
-            <ComplimentSystem />
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/letters" element={<LoveLettersPage />} />
-        <Route path="/gallery" element={<GalleryPage />} />
-        <Route path="/timeline" element={<TimelinePage />} />
-        <Route path="/poems" element={<PoemsQuotesPage />} />
-        <Route path="/games" element={<GamesPage />} />
-        <Route path="/future" element={<FuturePage />} />
-        <Route path="/favorites" element={<FavoritesPage />} />
-                <Route path="/her-corner" element={<HerCornerPage />} />
-      </Routes>
-    </div>
+    <ThemeProvider>
+      <div className="app page container">
+        <FloatingNav />
+        <ComplimentSystem />
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/letters" element={<LoveLettersPage />} />
+          <Route path="/gallery" element={<GalleryPage />} />
+          <Route path="/timeline" element={<TimelinePage />} />
+          <Route path="/poems" element={<PoemsQuotesPage />} />
+          <Route path="/games" element={<GamesPage />} />
+          <Route path="/future" element={<FuturePage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="/her-corner" element={<HerCornerPage />} />
+        </Routes>
+      </div>
+    </ThemeProvider>
   )
 }
 
